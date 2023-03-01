21a8218c6d8a9848c2994d6c5eb9a59930e01281
zluuba <zluyba.nikitina@gmail.com>
2023-03-01 10:13:33 +0300

    Fix: fix linter errors

--- page_loader/core.py
+++ page_loader/core.py
@@ -1,5 +1,5 @@
 from page_loader.common import get_filename, get_file_path
-import requests
+# import requests
 
 
 def download(url, path):
@@ -7,12 +7,12 @@ def download(url, path):
     file_path = get_file_path(path)
     full_path = file_path + file_name
 
-    response = requests.get(url, stream=True, timeout=10)
+    # response = requests.get(url, stream=True, timeout=10)
     try:
         print('\n' + 'All good, path: ')
         # with open(full_path, 'wb') as file:
-            # for data in response.iter_content():
-            #     file.write(data)
+        #     for data in response.iter_content():
+        #         file.write(data)
     except PermissionError:
         return f"Path {path} does not exist"
 
