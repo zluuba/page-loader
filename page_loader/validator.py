064eea7d1b9063a0d1a17fcd748e8c8715fb2887
zluuba <zluyba.nikitina@gmail.com>
2023-03-09 15:49:10 +0300

    Fit: add logger

--- page_loader/validator.py
+++ page_loader/validator.py
@@ -1,17 +1,23 @@
+from page_loader.log import get_logger
 import validators
 import requests
 import sys
 import os
 
+logger = get_logger(__name__)
+
 
 def validate_user_input(url, path):
     if not os.path.exists(path) or not os.path.isdir(path):
+        logger.critical(f"path does not exist: {path}")
         sys.exit(f"Directory doesn't exist: {path}.")
 
     if not os.access(path, os.W_OK):
+        logger.critical(f"have not access to path: {path}")
         sys.exit(f"You don't have access to write in: {path}")
 
     if not validators.url(url):
+        logger.critical(f"invalid url: {url}")
         sys.exit(f"Incorrect url: {url}")
 
 
@@ -22,6 +28,8 @@ def get_valid_response(url):
             raise ConnectionError
 
     except (requests.exceptions.ConnectionError, ConnectionError) as error:
+        logger.critical(f"connection error: {ConnectionError}, "
+                        f"url: {url}")
         sys.exit(f"Connection error: {error}. "
                  "Try again later or check that the URL is working correct.")
 
