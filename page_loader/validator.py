089adb732d004cf15e159c5cc3c6a75ff426f133
zluuba <zluyba.nikitina@gmail.com>
2023-03-09 16:15:54 +0300

    Fix: update validators

--- page_loader/validator.py
+++ page_loader/validator.py
@@ -10,40 +10,30 @@ logger = get_logger(__name__)
 def validate_user_input(url, path):
     if not os.path.exists(path) or not os.path.isdir(path):
         logger.critical("Directory not exists")
-        sys.exit(f"Directory doesn't exist: {path}.")
+        sys.exit(1)
 
     if not os.access(path, os.W_OK):
         logger.critical(f"Need permission to write in: {path}")
-        sys.exit(f"You don't have access to write in: {path}")
+        sys.exit(1)
 
     if not validators.url(url):
         logger.critical(f"Invalid url: {url}")
-        sys.exit(f"Incorrect url: {url}")
+        sys.exit(1)
 
 
 def get_valid_response(url):
     try:
         response = requests.get(url, stream=True, timeout=10)
         status_code = response.status_code
-        if 300 <= status_code < 400:
+        if 300 <= status_code < 600:
+            logger.debug(f'connection error: {status_code}')
             raise ConnectionError(
                 "Need further action to complete the request",
                 url
             )
-        if 400 <= status_code < 500:
-            raise ConnectionError(
-                "the request contains bad syntax or cannot be fulfilled",
-                url
-            )
-        if 500 <= status_code < 600:
-            raise ConnectionError(
-                "the server failed to fulfil an apparently valid request",
-                url
-            )
 
     except (requests.exceptions.ConnectionError, ConnectionError) as error:
-        logger.critical(f"{error.args[0]}, "
-                        f"url: {error.args[1]}")
+        logger.critical(f"{error.args[0]}, url: {error.args[1]}")
         sys.exit(1)
 
     return response
