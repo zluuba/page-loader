98211515eb9adbe1681fa145c87a5d87e1615dd4
zluuba <zluyba.nikitina@gmail.com>
2023-03-09 16:26:13 +0300

    Fix: update validators

--- page_loader/validator.py
+++ page_loader/validator.py
@@ -7,6 +7,18 @@ import os
 logger = get_logger(__name__)
 
 
+class RedirectError(Exception):
+    pass
+
+
+class ClientError(Exception):
+    pass
+
+
+class ServerError(Exception):
+    pass
+
+
 def validate_user_input(url, path):
     if not os.path.exists(path) or not os.path.isdir(path):
         logger.critical("Directory not exists")
@@ -25,15 +37,36 @@ def get_valid_response(url):
     try:
         response = requests.get(url, stream=True, timeout=10)
         status_code = response.status_code
-        if 300 <= status_code < 600:
-            logger.debug(f'connection error: {status_code}')
-            raise ConnectionError(
-                "Bad request",
+        if 300 <= status_code < 400:
+            raise RedirectError(
+                "Need further action to complete the request",
+                url
+            )
+        if 400 <= status_code < 500:
+            raise ClientError(
+                "the request contains bad syntax or cannot be fulfilled",
+                url
+            )
+        if 500 <= status_code < 600:
+            raise ServerError(
+                "the server failed to fulfil an apparently valid request",
                 url
             )
 
-    except (requests.exceptions.ConnectionError, ConnectionError) as error:
-        # logger.critical(f"{error.args[0]}, url: {error.args[1]}")
+    except requests.exceptions.ConnectionError as error:
+        logger.critical(f"{error.args[0]}, url: {error.args[1]}")
+        sys.exit(1)
+
+    except RedirectError as error:
+        logger.warning(f"{error.args[0]}, url: {error.args[1]}")
+        sys.exit(1)
+
+    except ClientError as error:
+        logger.critical(f"{error.args[0]}, url: {error.args[1]}")
+        sys.exit(1)
+
+    except ServerError as error:
+        logger.critical(f"{error.args[0]}, url: {error.args[1]}")
         sys.exit(1)
 
     return response
